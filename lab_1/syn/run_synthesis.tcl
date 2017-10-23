60cbc0008b5e306aba47dbaa6fd295cfe1a79004
Luca Sasselli <4eversax@gmail.com>
2017-10-23 20:53:41 +0200

    Added synthesis file

--- lab_1/syn/run_synthesis.tcl
+++ lab_1/syn/run_synthesis.tcl
@@ -2,13 +2,22 @@
 remove_design -designs
 
 # Load project
+define design lib WORK -path ./work
+set search path [list . /software/synopsys/syn current/libraries/syn/software/dk/nangate45/synopsys ]
+set link library [list "*" "NangateOpenCellLibrary typical ecsm.db" "dw foundation.sldb" ]
+set target library [list "NangateOpenCellLibrary typical ecsm.db" ]
+set synthetic library [list "dw foundation.sldb" ]
+
 analyze -library WORK -format vhdl {../hdl/filter_top.v}
 
+# Preserve RTL name in power consumtpion estimation
+set_power_preserve_rtl_hier_names true
+
+# Analyze HDL
 elaborate filter_top -architecture behavioral -library WORK
 
+# Dumb compilation
 compile -exact_map
-
-create_clock -name "CLK" -period 2 CLK
 
 # Export preliminary netlist
 write -hierarchy -format vhdl -output netlist/free_synth_filter_top.vhdl
@@ -19,11 +28,22 @@ report_timing > report/free_time_filter_top.txt
 report_area > report/free_area_filter_top.txt
 
 # set constraints
-set_max_delay 2 -from [all_inputs] -to [all_outputs]
+create_clock -name MY_CLK -period 2 CLK
+set_dont_touch_network MY_CLK
+set_clock_uncertainty 0.07 [get_clocks MY_CLK]
+set_input_delay 0.5 -max -clock MY_CLK [remove_from_collection [all_inputs] CLK]
+set_output_delay 0.5 -max -clock MY_CLK [all_outputs]
+set OLOAD [load_of NangateOpenCellLibrary/BUF_X4/A]
+set_load $OLOAD [all_outputs]
 
+# Smart compilation
 compile -map_effort high
 
 # Export optimized netlist
+ungroup -all -flatten
+change names -hierarchy -rules verilog
+write_sdf ../netlist/filter.sdf
+write sdc ../netlist/filter.sdc
 write -hierarchy -format vhdl -output netlist/opt_synth_filter_top.vhdl
 write -hierarchy -format verilog -output netlist/opt_time_filter_top.v
 
