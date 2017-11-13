140b7a9198aad0c5204f444a91562d0fda80a669
Luca Sasselli <4eversax@gmail.com>
2017-11-13 18:38:50 +0100

    Added encounter power analysis

--- lab_1b/run-all.sh
+++ lab_1b/run-all.sh
@@ -5,7 +5,6 @@ set -e
 # Compile HDL for initial simulation
 cd hdl
 source /software/scripts/init_msim6.2g
-make clean
 make init
 make all
 vsim -c work.tb_filter -do "run -all"
@@ -18,8 +17,8 @@ dc_shell-xg-t -f run-synthesis-free.tcl
 dc_shell-xg-t -f run-synthesis-opt.tcl
 dc_shell-xg-t -f prepare-saif.tcl
 
-# Compute switching activity
-cd ../sim
+# Compute pre place-and-route switching activity
+cd ../switch-pre
 source /software/scripts/init_msim6.2g
 make clean
 make init
@@ -31,13 +30,20 @@ cd ../syn
 source /software/scripts/init_synopsys
 dc_shell-xg-t -f run-power.tcl
 
-# Run physical synthesis
+# Run place and route
 cd ../soce
 source /software/scripts/init_edi13
-encounter -nowin -init run-encounter.tcl
+encounter -nowin -init run-placeroute.tcl
 
+# Compute pre place-and-route switching activity
+cd ../switch-post
+source /software/scripts/init_msim6.2g
+make clean
+make init
+make all
+vsim -c -L /software/dk/nangate45/verilog/msim6.2g -sdftyp /tb_filter/DUT=../soce/filter_top.sdf work.tb_filter -do "vcd filter_top.vcd; vcd add /tb_filter/DUT/*; run -all"
 
-
-
-
-
+# Run power analysis
+cd ../soce
+source /software/scripts/init_edi13
+encounter -nowin -init run-power.tcl
